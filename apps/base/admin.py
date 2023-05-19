0a97f14340cc921b17ff9aead4914d1f5c73ad32
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-05-19 16:15:39 +0600

    Registering the model admin

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 # my imports
-from .models import Settings 
+from .models import Settings,About
 # Register your models here.
 
 class SettingsFilterAdmin(admin.ModelAdmin):
     list_filter = ('title', )
     list_display = ('title', 'descriptions')
     search_fields = ('title', 'descriptions')
+
+class AboutFilterAdmin(admin.ModelAdmin):
+    list_filter = ('title', )
+    list_display = ('title', 'descriptions')
+    search_fields = ('title', 'descriptions')
     
 admin.site.register(Settings, SettingsFilterAdmin)
+admin.site.register(About, AboutFilterAdmin)
--- apps/base/views.py
+++ apps/base/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 # my imports
-from .models import Settings
+from .models import Settings,About
 # Create your views here.
 
 def index(request):
     setting = Settings.objects.latest("id")
-    
     return render(request, "base/index.html",locals())
+
+def about(request):
+    about = About.objects.latest("id")
+    return render(request, "base/about.html",locals())
+    
