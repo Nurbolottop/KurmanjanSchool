51e65459fd83c6859b2bd34ba05c62c4a648046d
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-05-19 16:11:50 +0600

    Create model for about

--- apps/base/models.py
+++ apps/base/models.py
@@ -13,7 +13,7 @@ class Settings(models.Model):
         force_format="WEBP", 
         quality=100, 
         upload_to='logo/',
-        verbose_name="Фотография для логотипа",
+        verbose_name="Логотип",
         blank = True, null = True
     )
     phone = models.CharField(
@@ -47,3 +47,26 @@ class Settings(models.Model):
     class Meta:
         verbose_name = "Мектеп боюнча маалымат"
         verbose_name_plural = "Мектеп боюнча маалыматтар"
+
+class About(models.Model):
+    title = models.CharField(
+        max_length=255,
+        verbose_name="Биз жонундо кыскача маалымат"
+    )
+    descriptions = models.TextField(
+        verbose_name="Биз жонундо кененрээк маалымат"
+    )
+    image = ResizedImageField(
+        force_format = "WEBP",
+        quality = "100",
+        upload_to = "about_image",
+        verbose_name="Биз жонундо сурот",
+        blank = True,null = True    
+    )
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Биз жонундо"
+        verbose_name_plural = "Биз жонундо"
