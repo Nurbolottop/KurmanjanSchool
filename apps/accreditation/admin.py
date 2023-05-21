c4116c8fae260a8dd2e0f6d9f7c3d544e1c5817b
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-05-21 16:29:19 +0600

    Models registrate

--- apps/accreditation/admin.py
+++ apps/accreditation/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
+# my imports
+from .models import Institutional,InstitutionalDetail,Software,SoftwareDetail
 # Register your models here.
+
+class InstitutionalFilterAdmin(admin.ModelAdmin):
+    list_filter = ('name', )
+    list_display = ('name', )
+    search_fields = ('name', )
+
+class InstitutionalDetailFilterAdmin(admin.ModelAdmin):
+    list_filter = ('name', )
+    list_display = ('name', )
+    search_fields = ('name', )
+    
+class SoftwareFilterAdmin(admin.ModelAdmin):
+    list_filter = ('name', )
+    list_display = ('name', )
+    search_fields = ('name', )
+    
+class SoftwareDetailFilterAdmin(admin.ModelAdmin):
+    list_filter = ('name', )
+    list_display = ('name', )
+    search_fields = ('name', )
+    
+admin.site.register(Institutional,InstitutionalFilterAdmin)
+admin.site.register(InstitutionalDetail,InstitutionalDetailFilterAdmin)
+admin.site.register(Software,SoftwareFilterAdmin)
+admin.site.register(SoftwareDetail,SoftwareDetailFilterAdmin)
