91d0975e56a4f14cb2b889bca1b8074535e05371
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-05-22 13:58:30 +0600

    Fixing bugs

--- apps/accreditation/urls.py
+++ apps/accreditation/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 # my imports
-# from .views import 
+from .views import institunional,institunional_detail,software,software_detail
 
 urlpatterns = [
+    path('institunional/accreditation/', institunional, name="institunional"),
+    path('institunional/accreditation/<int:id>/', institunional_detail, name="institunional_detail"),
+    path('software/accreditation/', software, name="software"),
+    path('software/accreditation/<int:id>/', software_detail, name="software_detail"),
 ]
--- apps/accreditation/views.py
+++ apps/accreditation/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
+# my imports
+from apps.base.models import Settings
+from .models import Institutional,InstitutionalDetail,Software,SoftwareDetail
 # Create your views here.
+def institunional(request):
+    setting = Settings.objects.latest("id")
+    accreditation = Institutional.objects.all()
+    return render(request, "documents/inst_akred.html",locals())
+
+def institunional_detail(request,id):
+    setting = Settings.objects.latest("id")
+    accreditation = Institutional.objects.get(id = id)
+    return render(request, "detail_pages/inst_detail.html",locals())
+
+def software(request):
+    setting = Settings.objects.latest("id")
+    accreditation = Software.objects.all()
+    return render(request, "documents/prog_akred.html",locals())
+
+def software_detail(request,id):
+    setting = Settings.objects.latest("id")
+    accreditation = Software.objects.get(id = id)
+    return render(request, "detail_pages/prog_detail.html",locals())
