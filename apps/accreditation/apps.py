b62a65ab87fb446881ad01fedee0593f5493b780
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-05-22 13:59:27 +0600

    Fixing bugs

--- apps/accreditation/apps.py
+++ apps/accreditation/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AccreditationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.accreditation'
+    verbose_name = "Аккредитациялар"
